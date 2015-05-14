dab27a33380edc9c666ec3f87eb20c8a590dc688
Land <landtastic@gmail.com>
2015-05-14 01:47:58 -0700

    multiline autocomplete tests

--- advanced.js
+++ advanced.js
@@ -321,3 +321,145 @@ $("#playallsongsby-artist").autocomplete({
         });
     },
 });
+
+$( "#query" )
+      // don't navigate away from the field on tab when selecting an item
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        source: function( request, response ) {
+          var apiKey = 'AIzaSyDlcHPnr5gJr1_pBSvVSRtFudfpIUppfjM';
+          var query = request.term;
+          $.getJSON( "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q="+query+"&key="+apiKey+"&format=5&alt=json&callback=?", {
+            term: extractLast( request.term )
+          }, response );
+        },
+        search: function() {
+          // custom minLength
+          var term = extractLast( this.value );
+          if ( term.length < 2 ) {
+            return false;
+          }
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( "\n" );
+          return false;
+        }
+});
+
+/*
+$( "#tags" )
+  // don't navigate away from the field on tab when selecting an item
+  .bind( "keydown", function( event ) {
+    if ( event.keyCode === $.ui.keyCode.TAB &&
+        $( this ).autocomplete( "instance" ).menu.active ) {
+      event.preventDefault();
+    }
+  })
+  .autocomplete({
+    minLength: 0,
+    source: function( request, response ) {
+      // delegate back to autocomplete, but extract the last term
+      response( $.ui.autocomplete.filter(
+        availableTags, extractLast( request.term ) ) );
+    },
+    focus: function() {
+      // prevent value inserted on focus
+      return false;
+    },
+    select: function( event, ui ) {
+      var terms = split( this.value );
+      // remove the current input
+      terms.pop();
+      // add the selected item
+      terms.push( ui.item.value );
+      // add placeholder to get the comma-and-space at the end
+      terms.push( "" );
+      this.value = terms.join( ", " );
+      return false;
+    }
+  });
+});
+*/
+
+function split( val ) {
+  return val.split( /\n\s*/ );
+}
+function extractLast( term ) {
+  return split( term ).pop();
+}
+
+
+
+/*
+$().ready(function() {
+    function _leftMatch(string, area) {
+        return string.substring(0, area.selectionStart).match(/[\wäöüÄÖÜß]+$/)
+    }
+
+    function _setCursorPosition(area, pos) {
+        if (area.setSelectionRange) {
+            area.setSelectionRange(pos, pos);
+        } else if (area.createTextRange) {
+            var range = area.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }
+
+    $("#query").autocomplete({
+        position: { my : "right top", at: "right bottom" },
+        source: function(request, response){
+            var apiKey = 'AIzaSyDlcHPnr5gJr1_pBSvVSRtFudfpIUppfjM';
+            var query = request.term;
+            $.ajax({
+                url: "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q="+query+"&key="+apiKey+"&format=5&alt=json&callback=?",  
+                dataType: 'jsonp',
+                success: function(data, textStatus, request) { 
+                   response( $.map( data[1], function(item) {
+                        return {
+                            label: item[0],
+                            value: item[0]
+                        }
+                    }));
+                }
+            });
+        },
+        focus: function() {
+            // prevent value inserted on focus
+            return false;
+        },
+        // Insert the match inside the ui element at the current position by replacing the matching substring
+        select: function(event, ui) {
+            //alert("completing "+ui.item.value);},
+            var m = _leftMatch(this.value, this)[0];
+            var beg = this.value.substring(0, this.selectionStart - m.length);
+            this.value = beg + ui.item.value + this.value.substring(this.selectionStart, this.value.length);
+            var pos = beg.length + ui.item.value.length;
+            _setCursorPosition(this, pos);
+            return false;
+        },
+        search:function(event, ui) {
+            var m = _leftMatch(this.value, this);
+            return (m != null )
+        }
+    });
+})
+*/
