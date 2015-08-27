099e001c306cf613eb7870bbe4d6214bcd41e24c
Land <landtastic@gmail.com>
2015-08-27 14:43:18 -0700

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,57 @@
-// Gruntfile.js
-
-// our wrapper function (required by grunt and its plugins)
-// all configuration goes inside this function
 module.exports = function(grunt) {
 
-  // ===========================================================================
-  // CONFIGURE GRUNT ===========================================================
-  // ===========================================================================
   grunt.initConfig({
-
-    // get the configuration info from package.json ----------------------------
-    // this way we can use things like name and version (pkg.name)
     pkg: grunt.file.readJSON('package.json'),
-
-    // all of our configuration will go here
-
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      //files: ['test/**/*.html']
+      files: ['index.html', 'js/**/*']
+    },
+    jshint: {
+      //files: ['Gruntfile.js', 'js/*', 'src/**/*.js', 'test/**/*.js'],
+      files: ['js/**/*'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
   });
 
-  // ===========================================================================
-  // LOAD GRUNT PLUGINS ========================================================
-  // ===========================================================================
-  // we can only load these if they are in our package.json
-  // make sure you have run npm install so our app can find these
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
 
 };
